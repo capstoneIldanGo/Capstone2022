e9f85eae6ff7d6e681984b148cf68838d4cd6ba1
Sujin Kim <sujinkim919@gmail.com>
2022-04-09 09:30:08 +0900

    하단 bottom sheet 메뉴 추가

---
+++ app/src/main/java/com/example/capstone/BottomSheetFragment.kt
@@ -0,0 +1,28 @@
+package com.example.capstone
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetFragment() : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.bottom_sheet, container, false)
+
+    companion object {
+        const val TAG = "BottomSheet"
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+}
--- app/src/main/java/com/example/capstone/MainActivity.kt
+++ app/src/main/java/com/example/capstone/MainActivity.kt
@@ -2,21 +2,30 @@ package com.example.capstone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.LinearLayout
+import androidx.core.view.MotionEventCompat
 import com.example.capstone.databinding.ActivityMainBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
 
     private var _binding: ActivityMainBinding ?= null
     private val binding get() = _binding!!
+    private val bottomSheet = BottomSheetFragment()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        bottomSheet.show(supportFragmentManager, BottomSheetFragment.TAG)
+
         binding.btnSearch.setOnClickListener {
 
         }
 
     }
+
 }
