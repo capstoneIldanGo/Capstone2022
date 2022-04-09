b9d2c6013c850e4f6604c957899c55bcb901271d
Sujin Kim <sujinkim919@gmail.com>
2022-04-09 17:23:54 +0900

    bottom sheet 디자인 (radius, margin)

--- app/src/main/java/com/example/capstone/BottomSheetFragment.kt
+++ app/src/main/java/com/example/capstone/BottomSheetFragment.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class BottomSheetFragment() : BottomSheetDialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(BottomSheetDialogFragment.STYLE_NORMAL, R.style.AppBottomSheetDialogTheme)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.bottom_sheet, container, false)
+    ): View = inflater.inflate(R.layout.bottom_sheet, container, false)
 
     companion object {
         const val TAG = "BottomSheet"
@@ -20,6 +26,7 @@ class BottomSheetFragment() : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
 
 
