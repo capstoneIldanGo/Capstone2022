219efe48f8d5e21f6183e6881e54f372a43cd47f
Sujin Kim <sujinkim919@gmail.com>
2022-04-09 20:23:23 +0900

    swipe event 적용

--- app/src/main/java/com/example/capstone/MainActivity.kt
+++ app/src/main/java/com/example/capstone/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.capstone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
 import android.view.MotionEvent
 import android.widget.LinearLayout
 import androidx.core.view.MotionEventCompat
@@ -14,17 +15,20 @@ class MainActivity : AppCompatActivity() {
     private val binding get() = _binding!!
     private val bottomSheet = BottomSheetFragment()
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        bottomSheet.show(supportFragmentManager, BottomSheetFragment.TAG)
-
         binding.btnSearch.setOnClickListener {
 
         }
+
+        binding.viewUnderSearchBar.setOnTouchListener(object:OnSwipeTouchListener(this@MainActivity) {
+            override fun onSwipeTop() {
+                bottomSheet.show(supportFragmentManager, BottomSheetFragment.TAG)
+            }
+        })
 
     }
 
