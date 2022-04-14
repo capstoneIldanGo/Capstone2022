45a49125eb776e98958344c9bd0ea6b43af707e3
RightHennessy <myheaven_b@naver.com>
2022-04-14 17:44:59 +0900

    add : 결과창으로 이동

--- app/src/main/java/com/example/capstone/MainActivity.kt
+++ app/src/main/java/com/example/capstone/MainActivity.kt
@@ -1,28 +1,41 @@
 package com.example.capstone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.GestureDetector
-import android.view.MotionEvent
+import android.view.*
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.core.view.MotionEventCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.capstone.databinding.ActivityMainBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
+
 class MainActivity : AppCompatActivity(), BottomSheetClickListener {
+
 
     private var _binding: ActivityMainBinding ?= null
     private val binding get() = _binding!!
     private val bottomSheet = BottomSheetFragment()
+    private val searchResult = SearchResultFragment()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnSearch.setOnClickListener {
+      //  val navController = Navigation.findNavController(binding.navHostActivityMain)
 
+
+        binding.btnSearch.setOnClickListener {
+            val nextIntent = Intent(this, ResultActivity::class.java)
+            startActivity(nextIntent)
+            //   navController.navigate(R.id.action_mainActivity_to_searchResultFragment)
         }
 
         binding.viewUnderSearchBar.setOnTouchListener(object:OnSwipeTouchListener(this@MainActivity) {
