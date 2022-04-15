ed94c0273281ab58ede12097250354b9f688d864
RightHennessy <myheaven_b@naver.com>
2022-04-15 15:08:42 +0900

    화면추가

---
+++ app/src/main/java/com/example/capstone/ResultDetailActivity.kt
@@ -0,0 +1,18 @@
+package com.example.capstone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.capstone.databinding.SearchDetailBinding
+
+class ResultDetailActivity : AppCompatActivity(){
+
+    private var _binding: SearchDetailBinding?= null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = SearchDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
