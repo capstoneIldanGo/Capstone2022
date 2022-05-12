3ff2fc73a274f3d3b51b898874d696e0d956d877
Sujin Kim <sujinkim919@gmail.com>
2022-05-12 20:19:00 +0900

    FEAT: 가격 알림 dialog 구현 및 버튼 이벤트 등록

--- app/src/main/java/com/ildango/capstone/result/AlarmDialog.kt
+++ app/src/main/java/com/ildango/capstone/result/AlarmDialog.kt
@@ -2,13 +2,15 @@ package com.ildango.capstone.result
 
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.Window
-import com.ildango.capstone.R
 import com.ildango.capstone.databinding.DialogSetAlarmBinding
 
-class AlarmDialog(context: Context) : Dialog(context) {
+class AlarmDialog(context: Context) : Dialog(context), View.OnClickListener {
 
     private var _binding: DialogSetAlarmBinding ?= null
     private val binding  get() = _binding!!
@@ -20,6 +22,22 @@ class AlarmDialog(context: Context) : Dialog(context) {
         setCanceledOnTouchOutside(false)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(binding.root)
+        window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        binding.btnCancelMakingAlarm.setOnClickListener(this)
+        binding.btnSubmitAlarm.setOnClickListener(this)
     }
 
+    override fun onClick(p0: View?) {
+        when(p0) {
+            binding.btnCancelMakingAlarm-> {
+                dismiss()
+            }
+            binding.btnSubmitAlarm-> {
+                val price = binding.editTextGetPrice.text
+                // price 값 전달 필요
+                dismiss()
+            }
+        }
+    }
 }
