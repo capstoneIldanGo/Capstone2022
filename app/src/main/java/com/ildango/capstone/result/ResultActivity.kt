95456a835551e8af225219d0e0e0673c3fd79497
RightHennessy <myheaven_b@naver.com>
2022-05-19 14:06:39 +0900

    CHORE : 함수이동 및 viewpager 기능 추가

--- app/src/main/java/com/ildango/capstone/result/ResultActivity.kt
+++ app/src/main/java/com/ildango/capstone/result/ResultActivity.kt
@@ -5,16 +5,14 @@ import android.os.Bundle
 import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
-import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.ildango.capstone.databinding.ActivitySearchResultBinding
 import com.ildango.capstone.resultdetail.ResultDetailActivity
-import com.ildango.capstone.resultdetail.SortingSheetFragment
 import kotlinx.android.synthetic.main.activity_search_result.*
+import kotlinx.android.synthetic.main.fragment_one_week_chart_page.*
 
 const val type1 = "내 주변"
 const val type2 = "전국"
@@ -28,20 +26,16 @@ class ResultActivity : AppCompatActivity() {
 
     private val priceListTags = arrayListOf(type1, type2, type3)
 
-    lateinit var lineData: LineData
-    lateinit var lineList: ArrayList<Entry>
-    lateinit var lineDataSet: LineDataSet
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivitySearchResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
         viewModel = ViewModelProvider(this).get(ResultViewModel::class.java)
 
+        vp_chart.adapter = ChartPagerAdapter(this)
         onClickListener()
         setPriceInfoList()
         setSearchView()
-        makeChart(10)
     }
 
     private fun setPriceInfoList() {
@@ -53,16 +47,17 @@ class ResultActivity : AppCompatActivity() {
     }
 
     private fun setListListener() {
-        binding.listviewPriceList.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            val nextIntent = Intent(this, ResultDetailActivity::class.java)
-            nextIntent.putExtra("keyword", intent.getStringExtra("keyword").toString())
-            when(position) {
-                0->nextIntent.putExtra("type", type1)
-                1->nextIntent.putExtra("type", type2)
-                2->nextIntent.putExtra("type", type3)
+        binding.listviewPriceList.onItemClickListener =
+            AdapterView.OnItemClickListener { _, _, position, _ ->
+                val nextIntent = Intent(this, ResultDetailActivity::class.java)
+                nextIntent.putExtra("keyword", intent.getStringExtra("keyword").toString())
+                when (position) {
+                    0 -> nextIntent.putExtra("type", type1)
+                    1 -> nextIntent.putExtra("type", type2)
+                    2 -> nextIntent.putExtra("type", type3)
+                }
+                startActivity(nextIntent)
             }
-            startActivity(nextIntent)
-        }
     }
 
     private fun setSearchView() {
@@ -91,46 +86,21 @@ class ResultActivity : AppCompatActivity() {
 
     private fun onClickListener() {
         binding.btnOneMonth.setOnClickListener {
-            makeChart(10)
-            //showall_viewpager.setCurrentItem
+            vp_chart.setCurrentItem(0, true)
         }
         binding.btnOneWeek.setOnClickListener {
-            makeChart(7)
+            vp_chart.setCurrentItem(1, true)
         }
     }
 
-    private fun makeChart(k : Int) {
-        lineChart.setNoDataText("데이터가 없습니다.")
-
-        setChart()
-
-        lineList = ArrayList()
-        // db에서 값 가져와 값 집어 넣기 : ArrayList<Int>()
-        //k개 만큼 배열 생성 하기
-        val priceList = arrayOf(700,300,200,1200,500,200,500)
-
-        for(i in priceList.indices){
-            lineList.add(Entry(i.toFloat(), priceList[i].toFloat()))
+    private class ChartPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+        override fun createFragment(position: Int): Fragment {
+            return when(position){
+                0->OneMonthChartFragment()
+                else->OneWeekChartFragment()
+            }
         }
 
-        lineDataSet = LineDataSet(lineList, null)
-        lineData = LineData(lineDataSet)
-        lineData.setValueTextSize(0f)
-        lineChart.data = lineData
-        lineChart.invalidate()
-    }
-
-    private fun setChart() {
-        lineChart.legend.isEnabled = false
-        lineChart.apply() {
-            setExtraOffsets(2f, 2f, 2f, 2f)
-            axisLeft.setDrawLabels(true)
-            axisRight.setDrawLabels(false)
-            description.isEnabled = false
-            isDoubleTapToZoomEnabled = false
-            isScaleYEnabled = false
-            isScaleXEnabled = false
-            xAxis.isEnabled = false
-        }
+        override fun getItemCount(): Int = 2
     }
 }
