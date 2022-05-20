179c459d6487d8eac2b37f28edadb09b87d487ee
Sujin Kim <sujinkim919@gmail.com>
2022-05-20 13:09:28 +0900

    FEAT: 상품 리스트 image - url로부터 set

--- app/src/main/java/com/ildango/capstone/resultdetail/ProductListAdapter.kt
+++ app/src/main/java/com/ildango/capstone/resultdetail/ProductListAdapter.kt
@@ -1,12 +1,15 @@
 package com.ildango.capstone.resultdetail
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.ildango.capstone.R
 import retrofit2.Response
+import java.lang.Exception
 
 class ProductListAdapter(private val items: MutableLiveData<Response<ProductItemList>>)
     : RecyclerView.Adapter<ProductViewHolder>() {
@@ -21,7 +24,18 @@ class ProductListAdapter(private val items: MutableLiveData<Response<ProductItem
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
         items.value!!.body()!!.productList.let { item ->
             with(holder) {
-                iv_thumbnail.setImageResource(R.drawable.logo)
+                try {
+                    Glide.with(holder.itemView.context)
+                        .load(item.get(position).productImage)
+                        .override(100,100)
+                        .fitCenter()
+                        .into(iv_thumbnail)
+                    Log.d("CheckUrl", item.get(position).productImage)
+                } catch (e:Exception) {
+                    Glide.with(holder.itemView.context)
+                        .load(R.drawable.logo)
+                        .into(iv_thumbnail)
+                }
                 tv_title.text = item.get(position).title
                 tv_price.text = "${item.get(position).price}원"
             }
