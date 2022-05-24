2812c2c8456bb267c115b529bac79b6c6fe35e27
Sujin Kim <sujinkim919@gmail.com>
2022-05-24 14:24:19 +0900

    CHORE: OKHttp Log 추가

--- app/src/main/java/com/ildango/capstone/data/service/RetrofitClient.kt
+++ app/src/main/java/com/ildango/capstone/data/service/RetrofitClient.kt
@@ -1,7 +1,9 @@
 package com.ildango.capstone.data.service
 
 import com.google.gson.GsonBuilder
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -10,12 +12,17 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
     private const val BASE_URL = "http://3.36.254.182/api/"
 
-    private val interceptor = HttpLoggingInterceptor()
-    private val client : OkHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
-    private val gson = GsonBuilder().setLenient().create()
+    private fun getInterceptor() : HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        return interceptor
+    }
 
-    private val instance : Retrofit by lazy {
-        Retrofit.Builder()
+    private fun getApiClient():Retrofit {
+        val client : OkHttpClient = OkHttpClient.Builder().addInterceptor(getInterceptor()).build()
+        val gson = GsonBuilder().setLenient().create()
+
+        return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(client)
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
@@ -24,13 +31,13 @@ object RetrofitClient {
     }
 
     val productApi: ProductService by lazy {
-        instance.create(ProductService::class.java)
+        getApiClient().create(ProductService::class.java)
     }
     val alarmApi: MyAlarmService by lazy {
-        instance.create(MyAlarmService::class.java)
+        getApiClient().create(MyAlarmService::class.java)
     }
     val wishApi: MyWishService by lazy {
-        instance.create(MyWishService::class.java)
+        getApiClient().create(MyWishService::class.java)
     }
 
 }
