d8b3e84517d225a0e8fc9421896198cbb18c1efe
Sujin Kim <sujinkim919@gmail.com>
2022-05-26 20:39:01 +0900

    FEAT: 찜리스트 오른쪽 스와이프 시 삭제버튼 고정

--- app/src/main/java/com/ildango/capstone/mypages/mywishlist/MyWishListActivity.kt
+++ app/src/main/java/com/ildango/capstone/mypages/mywishlist/MyWishListActivity.kt
@@ -2,7 +2,6 @@ package com.ildango.capstone.mypages.mywishlist
 
 import android.content.Intent
 import android.graphics.*
-import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.VectorDrawable
 import android.os.Bundle
 import android.view.View
@@ -43,56 +42,14 @@ class MyWishListActivity : AppCompatActivity() {
     }
 
     private fun setItemSwipe() {
-        val simpleItemTouchCallback: ItemTouchHelper.SimpleCallback = object :
-            ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
-
-            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
-                return false
-            }
-
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                adapter.removeItem(viewHolder.absoluteAdapterPosition)
-            }
-
-            override fun onChildDraw(
-                c: Canvas,
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                dX: Float,
-                dY: Float,
-                actionState: Int,
-                isCurrentlyActive: Boolean
-            ) {
-                val itemView = viewHolder.itemView
-                val height: Float = itemView.bottom.toFloat() - itemView.top.toFloat()
-                val width: Float = height / 3
-
-                val p = Paint()
-                p.color = ContextCompat.getColor(this@MyWishListActivity, R.color.deleteButton)
-                val bg = RectF(
-                    itemView.left.toFloat() + width,
-                    itemView.top.toFloat(),
-                    itemView.right.toFloat(),
-                    itemView.bottom.toFloat()
-                )
-                c.drawRoundRect(bg, 40F, 40F, p)
-
-                val icon: Bitmap =
-                    (getDrawable(R.drawable.ic_baseline_delete_24) as VectorDrawable).toBitmap()
-                val iconDest = RectF(
-                    itemView.right.toFloat() - 2 * width,
-                    itemView.top.toFloat() + width,
-                    itemView.right.toFloat() - width,
-                    itemView.bottom.toFloat() - width
-                )
-
-                c.drawBitmap(icon, null, iconDest, p)
-
-                super.onChildDraw(c,recyclerView,viewHolder,dX,dY,actionState,isCurrentlyActive)
-            }
+        val swipeItemCallback = SwipeItemCallback(this, adapter).apply {
+            setClamp(resources.displayMetrics.widthPixels.toFloat() / 5)
         }
-        val itemTouchHelper = ItemTouchHelper(simpleItemTouchCallback)
-        itemTouchHelper.attachToRecyclerView(binding.recyclerviewWishList)
+        ItemTouchHelper(swipeItemCallback).attachToRecyclerView(binding.recyclerviewWishList)
+        binding.recyclerviewWishList.setOnTouchListener { _, _ ->
+            swipeItemCallback.removePreviousClamp(binding.recyclerviewWishList)
+            false
+        }
     }
 
     private fun setRecyclerview() {
