1297fdcaaec7d2167d8001a51b575203d031f18f
Sujin Kim <sujinkim919@gmail.com>
2022-05-26 14:12:52 +0900

    FEAT: 팔리지 않은 제품만 표시, 플랫폼 필터링 추가

--- app/src/main/java/com/ildango/capstone/data/repository/ProductRepository.kt
+++ app/src/main/java/com/ildango/capstone/data/repository/ProductRepository.kt
@@ -1,25 +1,30 @@
 package com.ildango.capstone.data.repository
 
-import android.util.Log
 import com.ildango.capstone.resultdetail.ProductItemList
 import com.ildango.capstone.data.service.RetrofitClient
 import retrofit2.Response
 import java.lang.Exception
 
 class ProductRepository {
+
+    private val platforms = listOf("Joongonara", "Bunjang")
+
     suspend fun getAllProduct(
         keyword: String,
         order: String,
         page: Int,
-        tag: List<Boolean>
+        tag: List<Boolean>,
+        platform: List<Boolean>
     ): Result<ProductItemList> {
 
         val data: Response<ProductItemList>
 
         return try {
             val mint = if(tag[1]) true else null
+            val area = if(tag[0]) true else null
+            val platform = getPlatform(platform)
 
-            data = RetrofitClient.productApi.getAllProduct(keyword, order, page, mint = mint)
+            data = RetrofitClient.productApi.getAllProduct(keyword=keyword, order=order, page=page, mint = mint, platform = platform)
 
             if (data.isSuccessful) {
                 data.body()?.let {
@@ -31,5 +36,16 @@ class ProductRepository {
         } catch (e: Exception) {
             Result.failure(Throwable(e.message))
         }
+    }
+
+    private fun getPlatform(platform: List<Boolean>): String? {
+        return if(platform[0]&&platform[1])
+            null
+        else if(platform[0])
+            platforms[0]
+        else if(platform[1])
+            platforms[1]
+        else
+            null
     }
 }
--- app/src/main/java/com/ildango/capstone/resultdetail/ResultDetailViewModel.kt
+++ app/src/main/java/com/ildango/capstone/resultdetail/ResultDetailViewModel.kt
@@ -25,7 +25,7 @@ class ResultDetailViewModel(private val productRepository: ProductRepository)
 
     fun getData(keyword:String, page:Int) {
         viewModelScope.launch {
-            productRepository.getAllProduct(keyword, productOrderType.value!!, page, productTag.value!!)
+            productRepository.getAllProduct(keyword, productOrderType.value!!, page, productTag.value!!, productPlatform.value!!)
                 .onSuccess {
                     productList.addAll(it.productList)
                     _product.value = productList
